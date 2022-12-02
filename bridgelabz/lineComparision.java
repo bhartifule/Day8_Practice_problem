1b7a16c4d14831cfa81ef230238e31e352f92c0b
Bharti <bharatifule10gmail.com>
2022-12-02 23:33:12 +0530

    line comparision added

--- bridgelabz/lineComparision.java
+++ bridgelabz/lineComparision.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
+import static java.lang.Math.sqrt;
 
 public class lineComparision {
-public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparision");
-}
-}
+
+	    static int x1 = 2;
+	    static int x2 = 5;
+	    static int y1 = 3;
+	    static int y2 = 5;
+	    public static void main(String[] args) {
+
+	        System.out.println("Welcome to Line Comparison Computation Program");
+
+
+	       double LengthOfLine = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+	        System.out.println("Length of line is "+LengthOfLine );
+	    }
+	}
+
+
